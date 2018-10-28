e2ce0c6b956952b6aa030e8ec377f3184def36d2
Marc <marc@storck.lu>
2018-10-28 14:13:23 +0100

    cleaning and adding relay control on BCM 23 & 24
    cleaning up the code
    adding relay control on GPIO BCM 23 & GPIO BCM 24

--- play.py
+++ play.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
 
-
 import os
 import subprocess
 import time
 import RPi.GPIO as GPIO
 
-
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
 
-GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-os.system('mpg123 /home/system.mp3')
+os.system('mpg123 /home/pi/Mp3Player-master/system.mp3')
+
 while True:
 
       if GPIO.input(17) ==0:
-                      print ("Pulse")      
-                      os.system('/home/mp3.sh')
-                      time.sleep(1)
+
+            print ("Pulse")
+		GPIO.setup(23, GPIO.OUT, initial=GPIO.HIGH)
+		GPIO.setup(24, GPIO.OUT, initial=GPIO.HIGH)
+            os.system('/home/pi/Mp3Player-master/mp3.sh')
+            GPIO.setup(23, GPIO.IN)
+            GPIO.setup(24, GPIO.IN)
+            time.sleep(1)
 
       time.sleep(0.5)
-GPIO.cleanup()
 
-#find -name "*.mp3" | sort --random-sort| head -n 5|xargs -d '\n' mpg123
+GPIO.cleanup()
